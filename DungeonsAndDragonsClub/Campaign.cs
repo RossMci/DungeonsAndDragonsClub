535682bba786140f0aeb8bc36203266ad0d5135c
Ross Mcinerney <rossmci@users.noreply.github.com>
2020-07-15 21:42:21 +0100

    users

--- DungeonsAndDragonsClub/Campaign.cs
+++ DungeonsAndDragonsClub/Campaign.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DungeonsAndDragonsClub
 {
-	interface Campaign
+	public interface Campaign
 	{
 	}
 }
---
+++ DungeonsAndDragonsClub/UserGroup.cs
@@ -0,0 +1,14 @@
+﻿namespace DungeonsAndDragonsClub
+{
+    using System;
+	using System.Collections.Generic;
+    /// <summary>
+    /// User Group to assign roles and permissions
+    /// </summary>
+	public interface UserGroup
+    {
+        Int32 Id { get; set; }
+        String Name { get; set; }
+        IList<UserAccount> Users { get; set; }
+    }
+}
